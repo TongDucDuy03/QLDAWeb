6ab308c3d853801145085f6a6c2bae688394cd7b
dangpham11 <156592583+dangpham11@users.noreply.github.com>
2024-01-26 10:27:18 +0700

    frontend

--- FE/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import About from "./pages/About";
-test("renders learn react link", () => {
-  render(<About />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
