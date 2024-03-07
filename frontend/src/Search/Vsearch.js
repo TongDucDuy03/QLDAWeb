9a9b5a008e79407787e97ff0c970bb9b0aa0db93
camtyr <ducbuivan08@gmail.com>
2024-03-07 11:02:03 +0700

    update FE profile again

--- frontend/src/Search/Vsearch.js
+++ frontend/src/Search/Vsearch.js
@@ -7,7 +7,7 @@ const Vsearch = ({ listBooks }) => {
     if (userId != null) {
       try {
         await axios.post(
-          `http://doraemon.id.vn/api/Cart/?id=${productId}&userId=${userId}&increaseQuantity=${true}`
+          `http://localhost:5168/api/Cart/?id=${productId}&userId=${userId}&increaseQuantity=${true}`
         );
         alert("Product added to cart successfully!");
       } catch (error) {
